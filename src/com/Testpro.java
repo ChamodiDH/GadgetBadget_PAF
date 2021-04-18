d4c573e958a577c27a6537efb368dcdb454b3619
ChamodiDH <chamodi3797@gmail.com>
2021-04-18 10:53:27 +0530

    comitted to the master branch

---
+++ src/com/Testpro.java
@@ -0,0 +1,23 @@
+package com;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/pros")
+public class Testpro
+{
+
+@GET
+@Path("/")
+@Produces(MediaType.TEXT_HTML)
+public String readItems()
+ {
+ return "Helloooooooooooooooooooo world";
+ }
+}
